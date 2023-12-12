c271355221fe7c54d1c580f8c8acc1a63992ef42
soundariya-2212 <soundariyaramu2212@gmail.com>
2023-12-12 11:35:55 +0530

    Day3

--- Day3/myapp/src/App.js
+++ Day3/myapp/src/App.js
@@ -1,14 +1,23 @@
 import Nav from "./Components/Nav";
 import './assets/Css/Nav.css'
-function App()
-{
-  return(
+import './assets/Css/style.css'
+function App() {
+  return (
     <div>
-      <Nav/>
-      {/* <p>
-        Hello World
-      </p> */}
-      {/* <Footer/> */}
+      <Nav />
+      <div className='cardbox-container'>
+
+        <div class="cardBox">
+          <div class="card">
+            <div class="h4">Effile Tower</div>
+
+            <div class="content">
+              <div class="h3">HoW It Is FaMoUs?</div>
+              <p>Paris is known for its gorgeous, imposing monuments. These iconic structures, often an exemplar of a particular era in architecture, are one of the city's instantly recognizable elements.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- Day3/myapp/src/Components/Nav.js
+++ Day3/myapp/src/Components/Nav.js
@@ -3,15 +3,37 @@ function Nav()
     return(
         <>
         <div className="navbar">
+            <div>
+                <ul>
+                    <li><a href="/">PARIS</a></li>
+                </ul>
+            </div>
             <ul className="hi">
                <li><a href="/">HOME</a></li>
-               <li><a href="/">FIELDS</a></li> 
+               <li><a href="/">PLACES</a></li> 
+               <li><a href="/">SERVICES</a></li> 
                <li><a href="/">CONTACT</a></li> 
             </ul>
         </div>
-        <div class="hello">
-            <h2>Welcome to Paris</h2>
-        </div>
+                <div className="mad">
+                    <h2>ThE CiTy Of LiGhTS</h2>
+                </div>
+                <div class="hello">
+                    <h2>  Paris is not just a city for lovers
+                    but a city to fall in LOVE with.......</h2>
+                </div>
+                <div class="meow">
+                <button class="not">EXPLORE</button>
+                </div>
+                {/* <div>
+                    <p>
+                        I love Paris for the million reasons that everybody loves the city.
+                    </p>
+                    <p>
+                        It's an incredibly romantic and beautiful place.
+                    </p>
+                </div> */}
+                
         </>
         
     )
